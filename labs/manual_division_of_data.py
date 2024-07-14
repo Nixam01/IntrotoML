8600f8e76af0035eba3299436e7b7b4cc34afd03
Nixam01 <marlatawiec01@gmail.com>
2024-07-14 20:44:08 +0200

    end of numpy processing lesson

--- labs/manual_division_of_data.py
+++ labs/manual_division_of_data.py
@@ -31,8 +31,68 @@ arr
 newArr = arr[0]
 newArr
 
-newArr = arr[0,1]
+newArr = arr[1,2]
 newArr
 
-newArr = arr[0,2:4]
+newArr = arr[1,2:4]
 newArr
+
+
+newArr = arr[1,2:5]
+newArr
+
+newArr = arr[1,:]
+newArr
+
+newArr = arr[2,:]
+newArr
+
+newArr = arr[0:3,2]
+newArr
+
+newArr = arr[:3,2]
+newArr
+
+newArr = arr[:3,2:4]
+newArr
+
+newArr = arr[:,-1]
+newArr
+
+newArr = arr[:,:-1]
+newArr
+
+arr = np.arange(50).reshape(10,5)
+arr
+
+split_level = 0.2
+num_rows = arr.shape[0]
+split_border = split_level * num_rows
+
+arr = arr[:round(split_border),:]
+arr = arr[round(split_border):,:]
+
+np.random.shuffle(arr)
+arr
+
+arr[:round(split_border),:]
+arr[round(split_border):,:]
+
+data = np.arange(500).reshape(100,5)
+np.random.shuffle(data)
+data
+
+split_level = 0.2
+num_rows = data.shape[0]
+split_border = split_level * num_rows
+
+X_test = data[:round(split_border),:-1]
+X_train = data[round(split_border):,:-1]
+Y_test = data[:round(split_border),-1]
+Y_train = data[round(split_border):,-1]
+
+data.shape
+X_train.shape
+X_test.shape
+Y_train.shape
+Y_test.shape
